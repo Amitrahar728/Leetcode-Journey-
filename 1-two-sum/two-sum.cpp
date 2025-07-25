815bcd2e5023b3dcad5d585e584b8eee088d8441
Amitrahar728 <amitrahar728@gmail.com>
2025-07-25 22:09:52 +0530

    Time: 43 ms (28.89%) | Memory: 14.1 MB (63.61%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,17 +1,35 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-    map<int , int > mpp;
-    int n = nums.size();
-    for(int i =0; i<n; i++){
-        int num = nums[i];
-        int x = target-num ; 
-        if(mpp.find(x)!= mpp.end()){
-            return{mpp[x],i};
+        // sort(nums.begin(), nums.end());
+        // int ind  = lower_bound(nums.begin(), nums.end(), target)- nums.begin();
+        // // it will return index with equal to or if equal to this is not present it will return index of greater element than this index
+        // int i =0;
+        vector<int> ans;
+        // for(i =0; i<ind; i++){
+        //     int rem = target - nums[i];
+        //     for(int j = i+1; j<ind; j++){
+        //         if(nums[j] == rem ){
+        //             ans.push_back(i);
+        //             ans.push_back(j);
+        //         }
+        //     }
+        // }
+        // return ans;
+
+
+        for(int i =0; i<nums.size(); i++){
+            if(!ans.empty()) break;
+            int rem = target-nums[i];
+            for(int j =i+1; j<nums.size(); j++){
+                if(nums[j] == rem){
+                    ans.push_back(i);
+                    ans.push_back(j);
+                    break;
+                }
+            }
         }
-        mpp[num]=i;
+        return ans;
+        
     }
-    return {-1,-1};
-    
-    }
-}; 
+};
