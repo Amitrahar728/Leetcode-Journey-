e49f77be23381abcbadd19366f84a920fa179911
Amitrahar728 <amitrahar728@gmail.com>
2025-07-31 00:50:36 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (94.85%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,15 +1,60 @@
+// class Solution {
+// public:
+//     int search(vector<int>& nums, int target) {
+//         int n = nums.size();
+//         int low =0;
+//         int high = n-1;
+//         while(low<=high){
+//             int mid = low-(high+low)/2;
+//             if(target == nums[mid]) return mid;
+//             else if(nums[low]<=nums[mid]){
+//                 if(target<=nums[mid] && nums[low]<=target){
+//                 high = mid-1;
+//                 }
+//                 else{
+//                     low = mid+1;
+//                 }
+
+//             }
+//             else{
+//                 if(target<=nums[high] && nums[mid]<=target){
+//                 low = mid+1;
+//                 }
+//                 else{
+//                    high = mid-1;
+//                 }
+//             }
+//         }
+//         return -1;
+//     }
+// };
+
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        bool check = true;
-        for(int i=0 ; i<nums.size(); i++){
-      
-            if(nums[i]== target){
-                check = false;
-                return i ;
-                
+        int n = nums.size();
+        int low = 0, high = n - 1;
+        while (low <= high) {
+            int mid = low+(high-low)/2;
+            if (nums[mid] == target) return mid;
+
+             if(nums[low] <= nums[mid]){
+                 if(nums[low] <= target && target <= nums[mid]){
+                high = mid - 1;
+            }
+            else {
+                low = mid + 1;
             }
         }
-        return -1;
+        else { 
+            if (nums[mid] <= target && target <= nums[high]) {
+                low = mid + 1;
+            }
+            else{
+                high = mid - 1;
+            }
+        }
+    }
+    return -1;
     }
 };
