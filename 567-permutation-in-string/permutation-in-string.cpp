926c954a957c0408fb3690afda1e020dcec14f70
Amit <amitrahar728@gmail.com>
2025-08-29 13:22:37 +0530

    Time: 7 ms (47.99%) | Memory: 9.9 MB (66.08%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -1,46 +1,28 @@
 class Solution {
 public:
-    bool check (int a[26], int b[26]){
-        for(int i =0;i<26; i++){
-            if(a[i]!=b[i]){
-                return 0; 
-            }
-            
-
-        }
-        return 1;
-    }
     bool checkInclusion(string s1, string s2) {
-        int arr1[26] ={0};
-        for(int i =0; i<s1.length(); i++){
-            int index = s1[i]-'a';
-            arr1[index]++;
+        vector<int> arr(26,0);  // s1 k record k liye 
+        vector<int> arr2(26,0); // s2 k record k liye
+        // Agar koi bhi window m jakr occurences of s1 equal ho s2 k tohh yes we have to return true 
+        // r (front pointer) -> increase krte raho jabtak atleast utne element ajaye jitne s1 m hai 
+        // l(previous pointer) --> if baaki characters mil jaye but kuch character jyada ho to l ko try kro 
+        if(s1.length()>s2.length()){
+            return false;
         }
-        int i =0;
-        int w = s1.length();
-        int arr2[26] ={0};
-        while(i<w && i <s2.length()){
-            int index = s2[i]-'a';
-            arr2[index]++;
-            i++;
+        for(int i =0; i<s1.size(); i++){
+            arr[s1[i]-'a']++;
         }
-         
-         if(check(arr1,arr2)){
-            return 1;
-         }
-         while(i<s2.length()){
-            char newchar = s2[i];
-            int index = newchar -'a';
-            arr2[index]++;
-            char oldchar = s2[i-w];
-            index = oldchar -'a';
-            arr2[index]--;
-            i++;
 
-            if(check(arr1,arr2)){
-            return 1;
-         }
-         }
-         return 0;
+        int l = 0; int r = 0; 
+        int n = s2.size();
+        int m = s1.size();
+        for(int i =0; i<n; i++){
+            arr2[s2[i]-'a']++;
+            if(i>=m){
+                arr2[s2[i-m]-'a']--;
+            }
+            if(arr == arr2) return true;
+        }
+         return false;
     }
 };
