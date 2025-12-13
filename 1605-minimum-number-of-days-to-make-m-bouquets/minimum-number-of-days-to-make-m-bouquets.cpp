e073dc55fe57d8738ef79efd52b5b379e7418568
Amit <amitrahar728@gmail.com>
2025-12-13 10:00:28 +0530

    Time: 31 ms (16.78%) | Memory: 123.4 MB (6.22%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
     bool check(int mid , vector<int>  arr, int m , int k ){
-        for(int i =0; i<arr.size(); i++){
-            arr[i] = arr[i]-mid;
-            // cout<<arr[i]<<" ";
-        }
-        // cout<<endl;
         int cnt =0;
         int ans =0;
         for(int i =0; i<arr.size(); i++){
-            if(arr[i]<= 0){
+            if(arr[i]<= mid){
                 cnt++;
                 if(cnt == k){
                     cnt =0;
                     ans++;
                     if(ans == m){
-                        // cout<<"NOW I AM RETURNING TRUE";
-                        // cout<<endl;
                         return true;
                         
                     }
@@ -25,9 +18,7 @@ public:
             else{
                 cnt =0;
             }
-            // cout<<ans<<" ";
         }
-        // cout<<endl;
         return false;
     }
     int minDays(vector<int>& bloomDay, int m, int k) {
@@ -37,13 +28,9 @@ public:
         }
         int low = *min_element(bloomDay.begin() , bloomDay.end());
         int high = *max_element(bloomDay.begin(), bloomDay.end());
-        // cout<<low<<" "<<high<<endl;
         int ans;
         while(low<=high){
             int mid = (low+high)/2;
-            // cout<<"low"<<"->"<<low<<endl;
-            // cout<<"high"<<"->"<<high<<endl;
-            // cout<<mid<<endl;
             if(check(mid, bloomDay, m , k)){
                 ans = mid;
                 high = mid-1;
