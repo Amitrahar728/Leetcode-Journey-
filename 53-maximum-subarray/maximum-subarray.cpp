a822a8b90401d417ddb208eea53c34dfb1343c9f
Amit <amitrahar728@gmail.com>
2026-05-04 13:37:30 +0530

    Time: 0 ms (100.00%) | Memory: 74.6 MB (5.06%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,29 +1,22 @@
-#include <vector>
-#include <algorithm>
-#include <limits>
-
-using namespace std;
-
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        if (nums.empty()) {
-            return 0;
+        vector<int> prefixsum(nums.size());
+        prefixsum[0] =nums[0];
+        for(int i =1; i<nums.size(); i++){
+            prefixsum[i] = prefixsum[i-1]+nums[i];
         }
 
-        vector<int> ps(nums.size());
-        ps[0] = nums[0];
-        for (int i = 1; i < nums.size(); ++i) {
-            ps[i] = ps[i-1] + nums[i];
+        //  at any point sum = Prefix(r) - prefix(l-1) 
+        //  for maximizing this prefix(l-1) should be minimized, for minimizing it 
+        int ans = prefixsum[0];
+        
+        int mini =0;
+        for(int i=0; i<prefixsum.size(); i++){
+            int besttillnow = prefixsum[i]- mini;
+            mini = min(mini , prefixsum[i]);
+            ans = max(ans , besttillnow);
         }
-
-        int mx = nums[0];
-        int mn = 0;
-
-        for (int i = 0; i < ps.size(); ++i) {
-            mx = max(mx, ps[i] - mn);
-            mn = min(mn, ps[i]);
-        }
-        return mx;
+        return ans;
     }
 };
